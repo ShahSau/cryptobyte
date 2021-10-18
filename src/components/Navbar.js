366b2035a4bf73107daa7ac32058b927ebd48601
ShahSau <shahriarksaurov@gmail.com>
2021-10-19 01:20:45 +0300

    initial

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import {Link } from 'react-router-dom';
+import { HomeOutlined, MoneyOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+const Navbar = () => {
+    return (
+        <div className='nav-container'>
+            <div className='logo-container'>
+                <Avatar />
+                <Typography.Title level={2} className='logo'>
+                    <Link to='/'>CryptoByte</Link>
+                    
+                </Typography.Title>
+                {/* <Button className='menu-control-container'>
+
+                </Button> */}
+            </div>
+            
+        </div>
+    )
+}
+
+export default Navbar
